565b558a7571171b57488239795c06dad2d95ff8
lubnakhatri-06 <142771737+lubnakhatri-06@users.noreply.github.com>
2023-11-07 07:44:26 +0530

    .

---
+++ ProgramList1/P1_1a.cpp
@@ -0,0 +1,14 @@
+/*WAC++P to enter two numbers and find their sum.*/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"NAME : MEET MAKADIYA"<<endl;
+    cout<<"ERNO : 220130318036"<<endl;
+    cout<<"PROBLEM : WAC++P to enter two numbers and find their sum."<<endl;
+    int a,b;
+    cout<<"Enter number one ::";
+    cin>>a;
+    cout<<"Enter number two ::";
+    cin>>b;
+    cout<<"The sum is ::"<<a+b;
+}
---
+++ ProgramList1/P1_1c.cpp
@@ -0,0 +1,16 @@
+/*WAC++P to enter length and breadth of a rectangle and find it's perimeter & area.*/
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"NAME : MEET MAKADIYA"<<endl;
+    cout<<"ERNO : 220130318036"<<endl;
+    cout<<"PROBLEM : WAC++P to enter length and breadth of a rectangle and find it's perimeter & area."<<endl;
+    int l,b,p,a;
+    cout<<"Enter the length: ";
+    cin>>l;
+    cout<<"Enter the breadth: ";
+    cin>>b;
+    cout<<"Perimeter = "<<2*(l+b)<<endl;
+    cout<<"Area = "<<l*b<<endl;
+    return 0;
+}
